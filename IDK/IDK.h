c3bd91996ecc60ed8e54447d50f13173ad11d512
Archan <archan@klizos.com>
2019-09-13 15:33:50 +0530

    Initial project setup

---
+++ IDK/IDK.h
@@ -0,0 +1,19 @@
+//
+//  IDK.h
+//  IDK
+//
+//  Created by iOS Development on 9/13/19.
+//  Copyright © 2019 iOS Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IDK.
+FOUNDATION_EXPORT double IDKVersionNumber;
+
+//! Project version string for IDK.
+FOUNDATION_EXPORT const unsigned char IDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDK/PublicHeader.h>
+
+
