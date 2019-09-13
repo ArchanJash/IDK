940450f3581dbefdefa3b09b8281fb90e4f08dae
Archan <archan@klizos.com>
2019-09-13 16:36:27 +0530

    viewcontroller added

---
+++ IDK/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  IDK
+//
+//  Created by iOS Development on 9/13/19.
+//  Copyright © 2019 iOS Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
