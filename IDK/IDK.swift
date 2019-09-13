0027877d0a288fdd6a81ccbb9461e28df98a8113
Archan <archan@klizos.com>
2019-09-13 15:39:49 +0530

    added a file

---
+++ IDK/IDK.swift
@@ -0,0 +1,22 @@
+//
+//  IDK.swift
+//  IDK
+//
+//  Created by iOS Development on 9/13/19.
+//  Copyright © 2019 iOS Development. All rights reserved.
+//
+
+import Foundation
+public final class SwiftyLib {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
